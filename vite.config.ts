0bb1382e9a3ca979b2ab4d3d3fe8b6c110bd55ca
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2025-09-01 04:38:45 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+ plugins: [react(), tailwindcss()],
+ preview: {
+  allowedHosts: ["notes-1015851813434.asia-south1.run.app"],
+ },
+});
